5183f804b906282948b6b80255e8d3bc0f64c89d
= <=>
2022-01-02 12:59:02 +0200

    Add project and company models

---
+++ Models/Company.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace TheBugTracker.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        
+        [DisplayName("Company Name")]
+        public string Name { get; set; }
+
+        [DisplayName("Company Description")]
+        public string Description { get; set; }
+
+
+        //Navigation Properties
+
+        public virtual ICollection<BTUser> Members { get; set; }
+        public virtual ICollection<Project> Projects { get; set; }
+    }
+}
