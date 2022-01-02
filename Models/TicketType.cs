a2ec0d19fde070ad9217d78f18a2712d9d6062b6
= <=>
2022-01-02 10:47:39 +0200

    Add data models for tickets

---
+++ Models/TicketType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.Linq;
+
+namespace TheBugTracker.Models
+{
+    public class TicketType
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Type Name")]
+        public string Name { get; set; }
+    }
+}
