76050f76dc9c15844dc0429be0e6030abd6145c7
= <=>
2022-01-06 09:52:04 +0200

    CompanyInfo service started

---
+++ Services/BTCompanyInfoService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TheBugTracker.Services.Interfaces;
+using TheBugTracker.Models;
+using TheBugTracker.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace TheBugTracker.Services
+{
+    public class BTCompanyInfoService : IBTCompanyInfoService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BTCompanyInfoService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<List<BTUser>> GetAllMembersAsync(int companyId)
+        {
+            List<BTUser> result = new();
+            result = await _context.Users.Where(u => u.CompanyId == companyId).ToListAsync();
+            return result;
+        }
+
+        public async Task<List<Project>> GetAllProjectsAsync(int companyId)
+        {
+            List<Project> result =new();
+            result = await _context.Projects.Where(p => p.CompanyId == companyId)
+                                            .Include(p => p.Members)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.ProjectId)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.TicketStatus)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.TicketPriority)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.TicketType)
+                                            .Include(p => p.ProjectPriority)
+                                            .ToListAsync();
+            return result;
+        }
+
+        public Task<List<Ticket>> GetAllTicketsAsync(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Company> GetCompanyInfoByIdAsync(int? companyId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
