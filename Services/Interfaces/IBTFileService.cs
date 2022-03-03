87371c40c51c08349ce702972ef5bd98d2cb2d04
= <=>
2022-03-03 20:09:39 +0200

    Implement and register file service

---
+++ Services/Interfaces/IBTFileService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using System;
+using Microsoft.AspNetCore.Http;
+
+namespace TheBugTracker.Services.Interfaces
+{
+    public interface IBTFileService
+    {
+        public Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file);
+        public string ConvertByteArrayToFileAsync(byte[] fileData, string extension);
+        public string GetFileIcon(string file);
+        public string FormatFileSize(long bytes);
+    }
+}
