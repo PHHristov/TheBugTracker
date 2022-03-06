18903eb1566fc33001d8656c57373cd2fc046d36
= <=>
2022-03-06 14:13:51 +0200

    lookup service

---
+++ Services/BTLookupService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TheBugTracker.Data;
+using TheBugTracker.Models;
+using TheBugTracker.Services.Interfaces;
+
+namespace TheBugTracker.Services
+{
+    public class BTLookupService : IBTLookupService
+    {
+        private readonly ApplicationDbContext _context;
+        public BTLookupService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<ProjectPriority>> GetProjectPrioritiesAsync()
+        {
+            try
+            {
+                return await _context.ProjectPriorities.ToListAsync();
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public Task<List<TicketPriority>> GetTicketPrioritiesAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<TicketStatus>> GetTicketStatusesAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<TicketType>> GetTicketTypesAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
