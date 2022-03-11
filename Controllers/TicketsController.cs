640c5de81c88d789c7df3133af6baf795d295835
= <=>
2022-03-11 13:42:03 +0200

    ticket attacchemnt model + controller + view

--- Controllers/TicketsController.cs
+++ Controllers/TicketsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
@@ -21,18 +22,20 @@ namespace TheBugTracker.Controllers
         private readonly IBTProjectService _projectService;
         private readonly IBTLookupService _lookupService;
         private readonly IBTTicketService _ticketService;
+        private readonly IBTFileService _fileService;
 
-        public TicketsController(ApplicationDbContext context, UserManager<BTUser> userManager, IBTProjectService projectService, IBTLookupService lookupService, IBTTicketService ticketService)
-        {
-            _context = context;
-            _userManager = userManager;
-            _projectService = projectService;
-            _lookupService = lookupService;
-            _ticketService = ticketService;
-        }
+		public TicketsController(ApplicationDbContext context, UserManager<BTUser> userManager, IBTProjectService projectService, IBTLookupService lookupService, IBTTicketService ticketService, IBTFileService fileService)
+		{
+			_context = context;
+			_userManager = userManager;
+			_projectService = projectService;
+			_lookupService = lookupService;
+			_ticketService = ticketService;
+			_fileService = fileService;
+		}
 
-        // GET: Tickets
-        public async Task<IActionResult> Index()
+		// GET: Tickets
+		public async Task<IActionResult> Index()
         {
             var applicationDbContext = _context.Tickets.Include(t => t.DeveloperUser).Include(t => t.OwnerUser).Include(t => t.Project).Include(t => t.TicketStatus).Include(t => t.TicketType);
             return View(await applicationDbContext.ToListAsync());
@@ -170,6 +173,57 @@ namespace TheBugTracker.Controllers
             return View(ticket);
         }
 
+        //POST: AddTicketController
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddTicketAttachment([Bind("Id,FormFile,Description,TicketId")] TicketAttachment ticketAttachment)
+        {
+            string statusMessage;
+
+            if (ModelState.IsValid && ticketAttachment.FormFile != null)
+            {
+                ticketAttachment.FileData = await _fileService.ConvertFileToByteArrayAsync(ticketAttachment.FormFile);
+                ticketAttachment.FileName = ticketAttachment.FormFile.FileName;
+                ticketAttachment.FileContentType = ticketAttachment.FormFile.ContentType;
+
+                ticketAttachment.Created = DateTimeOffset.Now;
+                ticketAttachment.UserId = _userManager.GetUserId(User);
+
+                await _ticketService.AddTicketAttachmentAsync(ticketAttachment);
+                statusMessage = "Success: New attachment added to Ticket.";
+            }
+            else
+            {
+                statusMessage = "Error: Invalid data.";
+
+            }
+
+            return RedirectToAction("Details", new { id = ticketAttachment.TicketId, message = statusMessage });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddTicketComment([Bind("Id, TicketId, Comment")] TicketComment ticketComment)
+		{
+			if (ModelState.IsValid)
+			{
+				try
+				{
+                    ticketComment.UserId = _userManager.GetUserId(User);
+                    ticketComment.Created = DateTime.Now;
+
+                    await _ticketService.AddTicketCommetnAsync(ticketComment);
+				}
+				catch (Exception)
+				{
+
+					throw;
+				}
+			}
+
+            return RedirectToAction("Details", new { id = ticketComment.TicketId });
+        }
+
         // POST: Tickets/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
@@ -227,6 +281,18 @@ namespace TheBugTracker.Controllers
             }
 
             return View(ticket);
+        }
+        
+        //GET: ShowFile
+        public async Task<IActionResult> ShowFile(int id)
+        {
+            TicketAttachment ticketAttachment = await _ticketService.GetTicketAttachmentByIdAsync(id);
+            string fileName = ticketAttachment.FileName;
+            byte[] fileData = ticketAttachment.FileData;
+            string ext = Path.GetExtension(fileName).Replace(".", "");
+
+            Response.Headers.Add("Content-Disposition", $"inline; filename={fileName}");
+            return File(fileData, $"application/{ext}");
         }
 
         // POST: Archive/Delete/5
--- Models/TicketAttachment.cs
+++ Models/TicketAttachment.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.AspNetCore.Http;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using TheBugTracker.Extensions;
 
 namespace TheBugTracker.Models
 {
@@ -23,9 +24,12 @@ namespace TheBugTracker.Models
         public string Description { get; set; }
 
         [NotMapped]
+        [DisplayName("Select a file")]
         [DataType(DataType.Upload)]
+        [MaxFileSize(1024 * 1024)]
+        [AllowedExtensions(new string[] { ".jpg", ".png", ".doc", ".docx", ".xls", ".xlsx", ".pdf" })]
         public IFormFile FormFile { get; set; }
-        
+
         [DisplayName("File Name")]
         public string FileName { get; set; }
         
