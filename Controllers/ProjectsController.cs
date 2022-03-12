d91b22e4c901cee807ed7d18a6fc7010e9bb2dc2
= <=>
2022-03-12 20:36:59 +0200

    Unassigned projects controller + view + Service updated

--- Controllers/ProjectsController.cs
+++ Controllers/ProjectsController.cs
@@ -87,6 +87,18 @@ namespace TheBugTracker.Controllers
             return View(projects);
         }
 
+        //GET: UnassignedProjects
+
+        public async Task<IActionResult> UnassignedProjects()
+        {
+            int companyId = User.Identity.GetCompanyId().Value;
+            List<Project> projects = new();
+
+            projects = await _projectService.GetUnassignedProjectsAsync(companyId);
+
+            return View(projects);
+        }
+
         // GET: Projects/Details/5
         public async Task<IActionResult> Details(int? id)
         {
